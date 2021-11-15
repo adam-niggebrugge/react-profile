3491e6bb479c50511119a02cbeb849f69102a9c1
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-14 20:15:52 -0500

    dropped starter code template stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import background from "./images/subtle-prism.svg";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="cust-background">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
