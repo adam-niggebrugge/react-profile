1fdd4305620dfb408dcf634addbf80b90805af93
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-15 18:03:00 -0500

    revision to folder structure

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import logo from '../images/nai_logo192x192.png'
-import './NavTabs.css';
+import './styles/NavTabs.css';
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
@@ -12,49 +12,76 @@ function NavTabs({ currentPage, handlePageChange }) {
 
   
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
+    <div id="home" className="flip-banner-red">
+      <nav  id="navbar"  className="navbar banner-purple" role="navigation">
+        <div className="cust-center">
+          <img alt="logo" className="cust-logo" src={logo}/>
+        </div>
+        <a href="#home" data-target="nav-mobile" className="sidenav-trigger">
+          <i  className="material-icons">menu</i>
+        </a>
+      <div className="nav-wrapper container">
+
+    <ul className="banner-red hide-on-med-and-down">
+      <li className="cust-up-margin">
         <a
           href="#home"
           onClick={() => handlePageChange('Home')}
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'Home' ? 'nav-link active btn btn-cust' : 'nav-link btn btn-cust'}
         >
           Home
         </a>
       </li>
-      <li className="nav-item">
+      <li className="cust-up-margin">
         <a
           href="#about"
           onClick={() => handlePageChange('About')}
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'About' ? 'nav-link active btn btn-cust' : 'nav-link btn btn-cust'}
         >
           About
         </a>
       </li>
-      <li className="nav-item">
+      <li className="cust-up-margin">
         <a
           href="#blog"
           onClick={() => handlePageChange('Blog')}
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'Blog' ? 'nav-link active btn btn-cust' : 'nav-link btn btn-cust'}
         >
           Blog
         </a>
       </li>
-      <li className="nav-item">
+      <li className="cust-up-margin">
         <a
           href="#contact"
           onClick={() => handlePageChange('Contact')}
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'Contact' ? 'nav-link active btn btn-cust' : 'nav-link btn btn-cust'}
         >
           Contact
         </a>
       </li>
     </ul>
+    </div>
+    </nav>
+    <ul id="nav-mobile" className="sidenav cust-sidenav banner">
+          <li>
+              <a className="btn sidenav-close btn-cust" href="#home">Top</a>
+          </li>
+          <li>
+              <a  className="btn sidenav-close btn-cust" href="/personal">About</a>
+          </li>
+          <li>
+              <a className="btn sidenav-close btn-cust" href="/">Portfolio</a>
+          </li>
+          <li>
+              <a className="btn btn-cust sidenav-close" target="_blank" href="/images/niggebrugge_resume.pdf">Resume</a>
+          </li>
+        </ul>
+  </div>
   );
 }
 
