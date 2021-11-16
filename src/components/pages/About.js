0d830e0d10349c0db4ce72cc8a46e3d07ae11678
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-16 00:21:24 -0500

    added image and greeting to about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import '../styles/Header.css';
+import profilePic from '../../images/ProfilePic.jpg';
+
+
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
-  );
+      <div className="row">
+          <div className="col s12">
+            <h2 className="header center offset-s2 page-title">Hello! I am <span className="header-name">Adam Niggebrugge</span>
+            </h2>
+          </div>
+          <div className="col s12">
+            <div id="index-banner" className="parallax-container">
+              <div className="section no-pad-bot">
+                <div className="container">
+                    <div className="row right-aligned">
+                      <h5  className="col offset-s6 s6 banner-red">A Full Stack Web Developer</h5>    
+                    </div>
+                </div>
+                <img alt="developer" src={ profilePic } />
+                
+              </div>
+            </div>
+          </div>
+        </div>
+    );
 }
