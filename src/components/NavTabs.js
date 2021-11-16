d49ce6d6714b76a11080b1761ac5fb017c24ee05
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-15 20:20:41 -0500

    mui media card component added. revised file names from Projects to ProjectList

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -46,12 +46,12 @@ function NavTabs({ currentPage, handlePageChange }) {
       </li>
       <li className="cust-up-margin">
         <a
-          href="#blog"
-          onClick={() => handlePageChange('Blog')}
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Blog' ? 'nav-link active btn btn-cust' : 'nav-link btn btn-cust'}
+          href="#Projects"
+          onClick={() => handlePageChange('Projects')}
+          // Check to see if the currentPage is `Projects`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Projects' ? 'nav-link active btn btn-cust' : 'nav-link btn btn-cust'}
         >
-          Blog
+          Projects
         </a>
       </li>
       <li className="cust-up-margin">
@@ -69,16 +69,37 @@ function NavTabs({ currentPage, handlePageChange }) {
     </nav>
     <ul id="nav-mobile" className="sidenav cust-sidenav banner">
           <li>
-              <a className="btn sidenav-close btn-cust" href="#home">Top</a>
+            <a
+              href="#home"
+              onClick={() => handlePageChange('Home')}
+              // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+              // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+              className={currentPage === 'Home' ? 'nav-link active btn sidenav-close btn-cust' : 'nav-link btn sidenav-close btn-cust'}
+            >Home</a>
           </li>
           <li>
-              <a  className="btn sidenav-close btn-cust" href="/personal">About</a>
+            <a
+              href="#about"
+              onClick={() => handlePageChange('About')}
+              // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+              className={currentPage === 'About' ? 'nav-link active btn sidenav-close btn-cust' : 'nav-link btn sidenav-close btn-cust'}
+            >About</a>
           </li>
           <li>
-              <a className="btn sidenav-close btn-cust" href="/">Portfolio</a>
+              <a
+                href="#Projects"
+                onClick={() => handlePageChange('Projects')}
+                // Check to see if the currentPage is `Projects`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                className={currentPage === 'Projects' ? 'nav-link active btn btn-cust' : 'nav-link btn btn-cust'}
+              >Projects</a>
           </li>
           <li>
-              <a className="btn btn-cust sidenav-close" target="_blank" href="/images/niggebrugge_resume.pdf">Resume</a>
+              <a
+                href="#contact"
+                onClick={() => handlePageChange('Contact')}
+                 // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                className={currentPage === 'Contact' ? 'nav-link active btn btn-cust' : 'nav-link btn btn-cust'}
+              >Contact</a>
           </li>
         </ul>
   </div>
