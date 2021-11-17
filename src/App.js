146dfa6bd6e53c4226c249f2288a32d610ebd0ab
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-17 00:54:11 -0500

    basic revision to the base react components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+//Basic React set up
 import './App.css';
-import PortfolioContainer from "./components/PortfolioContainer";
-import Footer from "./components/Footer";
+//React component for navigation
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const App = () =>
-  <main className="cust-background">
-    <PortfolioContainer />;
+//Universal components to be rendered on all pages
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import NavTabs from "./components/NavTabs";
+
+//Pages to be displayed
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import ProjectList from "./pages/ProjectList";
+
+function App () {
+  return ( 
+    <Router> 
+      <Header />
+      <NavTabs />
+      <main className="cust-background">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/portfolio" component={ProjectList} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+      </main>
     <Footer />
-  </main>
+    </Router>
+  )
+}
 export default App;
