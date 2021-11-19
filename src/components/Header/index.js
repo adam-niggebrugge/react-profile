c3c497bda40e8e674e2a70fa331522aa20ae830e
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-19 00:07:55 -0500

    brought in title to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import Title from "../core/Title";
 
 const Header = () => {
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link className="text-light" to="/">
-          <h1 className="m-0">Tech Thoughts</h1>
-        </Link>
+        <Title>
+
+        </Title>
         <p className="m-0">Get into the mind of a programmer.</p>
       </div>
     </header>
