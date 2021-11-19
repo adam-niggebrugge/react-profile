c69e452ddf2d1f38ee403cf61e2dc9cb359bab13
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-19 16:27:50 -0500

    revised color scheme. make the background fit screen

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import "./Header.css";
 import Title from "../core/Title";
 
 const Header = (props) => {
   return (
     <header>
       <div className={props.data.headerBackground}>
-        <Title variant={props.data.style}>
+        <Title variant={props.data.style} className="header-name">
             {props.data.headerTitle}
         </Title>
         <p className="m-0">{props.data.text}</p>
