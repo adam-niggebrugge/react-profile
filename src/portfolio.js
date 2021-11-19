711fa08f78866a2b41f0ddf5c5d474b8edbda2d7
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-19 09:11:50 -0500

    revised dynamic data to have content to render based on page load

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,52 +1,75 @@
-const portfolio = [
-    {
-       title: "Trail Finder",
-       body: "As a team we collaborated on ideas for an app. We then researched available web service APIs to support the app idea. We used an agile methodology and Github Projects Board to organize ourselves. We opted for slack to serve as our daily stand up meetings. We completed this project within the week time frame we were given.",
-       siteLink: "https://jyothybaby.github.io/Trail-Finder/",
-       gitLink: "https://github.com/jyothybaby/Trail-Finder",
-       imagePath: "/images/nature.jpg",
-       altImageText: "Trail-Finder Landing Page",
-    },
-    {
-       title: "MVC Simple Blogging",
-       body: "MVC architecture is some of the quickest means to implement a \"FULL\" website. In this is an example of code deployed through Heroku servers to allow any user to create an account, create blogs that they alone can edit and to comment on any existing blog. The site was built with Handlebars extension from Express. This is also a NoSQL built site due to the simplicity of the models.",
-       siteLink: "https://nai-blog-cms.herokuapp.com/",
-       gitLink: "https://github.com/adam-niggebrugge/MVC_TechBlog",
-       imagePath: "/images/MVC_demo.gif",
-       altImageText: "Animated image of MVC website",
-    },    
-    {
-       title: "Weather Forecast",
-       body: "This app demonstrates use of a Server Side API Call. Uses HTML, CSS, JS, MomentJS and Boostrap. I was required to create an app that could provide weather results for any area searched by a user.",
-       siteLink: "https://adam-niggebrugge.github.io/Weather_City_App/",
-       gitLink: "https://github.com/adam-niggebrugge/Weather_City_App",
-       imagePath: "/images/CitySearched.jpg",
-       altImageText: "City Weather Dashboard",
-    },    
-    {
-       title: "NoSQL Fitness Tracker",
-       body: "",
-       siteLink: "",
-       gitLink: "",
-       imagePath: "",
-       altImageText: "",
-    },
-    {
-       title: "Javascript Quiz",
-       body: "A simple use of javascript for a user to enjoy a \"quiz/game\". Involves using the browser's local storage for retaining high scores. Uses Javascript, HTML, CSS with a little help from Bootstrap to implement a modal that records the user's name. What I enjoyed the most was returning the questions asked in a random order and also presenting the multiple choice answers in a random arrangement.",
-       siteLink: "https://adam-niggebrugge.github.io/Code_Quiz_WEB_APIs/",
-       gitLink: "https://github.com/adam-niggebrugge/Code_Quiz_WEB_APIs",
-       imagePath: "/images/CodeQuizRefined.gif",
-       altImageText: "Animated view of Quiz App",
-    },
-    {
-       title: "Password Generator",
-       body: "This exercise was a demonstration of handling user inputs to return a \"password\" that met those requested. This relies on client side Javascript. Makes use of Materialize for the formatting.",
-       siteLink: "https://adam-niggebrugge.github.io/03-Javascript-Password-Generator/",
-       gitLink: "https://github.com/adam-niggebrugge/03-Javascript-Password-Generator",
-       imagePath: "/images/password_generation.gif",
-       altImageText: "Animated of Password",
-    }
-]
+const portfolio = {
+   
+   home:{
+      headerTitle : "Welcome",
+      headerBackground: "red",
+      body: "a quick blurb of what this site is",
+
+   },
+   about:{
+      headerTitle : "On a Personal Note",
+      headerBackground: "blue",
+      body: "About me personal content goes here",
+   },
+   contact:{
+      headerTitle : "Let's get in Touch",
+      headerBackground: "grey",
+      body: "Might be nothing here",
+   },
+   projectList : {
+      headerTitle : "My Projects",
+      headerBackground: "green",
+      projects: [
+            {
+               title: "Trail Finder",
+               body: "As a team we collaborated on ideas for an app. We then researched available web service APIs to support the app idea. We used an agile methodology and Github Projects Board to organize ourselves. We opted for slack to serve as our daily stand up meetings. We completed this project within the week time frame we were given.",
+               siteLink: "https://jyothybaby.github.io/Trail-Finder/",
+               gitLink: "https://github.com/jyothybaby/Trail-Finder",
+               imagePath: "/images/nature.jpg",
+               altImageText: "Trail-Finder Landing Page",
+            },
+            {
+               title: "MVC Simple Blogging",
+               body: "MVC architecture is some of the quickest means to implement a \"FULL\" website. In this is an example of code deployed through Heroku servers to allow any user to create an account, create blogs that they alone can edit and to comment on any existing blog. The site was built with Handlebars extension from Express. This is also a NoSQL built site due to the simplicity of the models.",
+               siteLink: "https://nai-blog-cms.herokuapp.com/",
+               gitLink: "https://github.com/adam-niggebrugge/MVC_TechBlog",
+               imagePath: "/images/MVC_demo.gif",
+               altImageText: "Animated image of MVC website",
+            },    
+            {
+               title: "Weather Forecast",
+               body: "This app demonstrates use of a Server Side API Call. Uses HTML, CSS, JS, MomentJS and Boostrap. I was required to create an app that could provide weather results for any area searched by a user.",
+               siteLink: "https://adam-niggebrugge.github.io/Weather_City_App/",
+               gitLink: "https://github.com/adam-niggebrugge/Weather_City_App",
+               imagePath: "/images/CitySearched.jpg",
+               altImageText: "City Weather Dashboard",
+            },    
+            {
+               title: "NoSQL Fitness Tracker",
+               body: "",
+               siteLink: "",
+               gitLink: "",
+               imagePath: "",
+               altImageText: "",
+            },
+            {
+               title: "Javascript Quiz",
+               body: "A simple use of javascript for a user to enjoy a \"quiz/game\". Involves using the browser's local storage for retaining high scores. Uses Javascript, HTML, CSS with a little help from Bootstrap to implement a modal that records the user's name. What I enjoyed the most was returning the questions asked in a random order and also presenting the multiple choice answers in a random arrangement.",
+               siteLink: "https://adam-niggebrugge.github.io/Code_Quiz_WEB_APIs/",
+               gitLink: "https://github.com/adam-niggebrugge/Code_Quiz_WEB_APIs",
+               imagePath: "/images/CodeQuizRefined.gif",
+               altImageText: "Animated view of Quiz App",
+            },
+            {
+               title: "Password Generator",
+               body: "This exercise was a demonstration of handling user inputs to return a \"password\" that met those requested. This relies on client side Javascript. Makes use of Materialize for the formatting.",
+               siteLink: "https://adam-niggebrugge.github.io/03-Javascript-Password-Generator/",
+               gitLink: "https://github.com/adam-niggebrugge/03-Javascript-Password-Generator",
+               imagePath: "/images/password_generation.gif",
+               altImageText: "Animated of Password",
+            }
+         ]
+   }
+}
 
 export default portfolio;
