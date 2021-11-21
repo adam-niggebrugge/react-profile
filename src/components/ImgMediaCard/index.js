d3989447e8e58b185f2c70ada657a895a7b05b03
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-20 23:36:34 -0500

    cleaing up unused portions from template css import

--- src/components/ImgMediaCard/index.js
+++ src/components/ImgMediaCard/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import "materialize-css/dist/css/materialize.min.css";
 
+import "./ImgMediaCard.css";
 import LiveLink from "../LiveLink";
 import GitLink from "../GitLink";
 
@@ -12,12 +13,12 @@ export default function ImgMediaCard(props) {
         <img className="activator" src={props.project.imagePath} alt={props.project.altImageText} />
       </figure>
       <div className="card-content">
-        <span className="card-title activator grey-text text-darken-4">{props.project.title}<i className="material-icons right">more_vert</i></span>
+        <span className="card-title activator grey-text text-darken-4 banner">{props.project.title}<i className="material-icons right">more_vert</i></span>
         <LiveLink link={props.project.siteLink} />
       </div>
       <div className="card-reveal">
-        <span className="card-title grey-text text-darken-4">{props.project.title}<i className="material-icons right">close</i></span>
-        <p>{props.project.body}.</p>
+        <span className="card-title grey-text text-darken-4 banner-alt">{props.project.title}<i className="material-icons right">close</i></span>
+        <p className="light flow-text">{props.project.body}.</p>
         <div className="offset-s6">
           <p className="flow-text valign-wrapper">
             Learn More 
